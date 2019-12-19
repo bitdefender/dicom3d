25f16ab55b6f8a9f3735f8a1bda59da76159e1aa
Daniel Alexandru MIRCESCU <amircescu@bitdefender.com>
2019-12-19 15:59:22 +0200

    version 0.9.9 - published on PyPi

--- setup.py
+++ setup.py
@@ -1,39 +1,7 @@
 import setuptools
 
-_long_description_ = """
-**dicom3d** is a comprehensive Python package for reconstructing arbitrary defined 3D sections 
-from volumetric medical scans (CTs or RMNs), scale accurate and with a builtin pixel-to-world 
-coordinate mapping system.
-
-It comes preloaded with the necessary mathematical backend to manipulate space information from 
-medical scans, and provides a mapping system that can transparently handle different imaging 
-properties such as pixel density, dataset thickness, patient orientation, patient positioning, 
-etc.
-
-Dependencies
-------------
-It relies on **pydicom** for loading DICOM medical images and **numpy** for array manipulation.
-
-Repository
-----------
-
-For source code and documentation visit [dicom3d](https://github.com/bitdefender/dicom3d) **GitHub** repository
-
-Installation
-------------
-
-From **PyPi** package repository:
-
-    pip install dicom3d
-
-License
--------
-Licence for this Python package is the **MIT License** under **Copyright (c) 2020 Bitdefender**.
-
-Author
--------
-Developed and maintained by *Alex Mircescu* (`amircescu`@`bitdefender.com`)
-"""
+with open("README_PKG.md", "r") as fh:
+    _long_description_ = fh.read()
 
 _description_ = \
     "Library for reconstructing arbitrary defined 3D sections" \
@@ -41,7 +9,7 @@ _description_ = \
 
 setuptools.setup(
     name="dicom3d",
-    version="0.9.8",
+    version="0.9.9",
     author="Alex Mircescu",
     author_email="mircescu@gmail.com",
     description= _description_,
@@ -49,6 +17,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/bitdefender/dicom3d",
     packages=setuptools.find_packages(),
+    include_package_data=True,
     keywords = [ 
         'python', 
         'medical', 'rmn', 'ct', 
